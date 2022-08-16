5cd3afe435be4e67cfb72e8fb710d7949cf94fa6
y_sugiyama_gxp <y.sugiyama@gxp.co.jp>
2022-08-16 19:00:54 +0900

    temp

--- gashimemo.py
+++ gashimemo.py
@@ -2,10 +2,8 @@ import os
 import text
 
 print(text.title);
-name = input();
 
-print(f"\nOK! これで始めるよ!! {name}")
-print("では次に本作のヒロインを選んでね。")
+print("まず本作のヒロインを選んでね。")
 input()
 
 
@@ -41,8 +39,65 @@ match select:
     case "2":
         print("「そうだね。。。」")
         input()
-        print(text.game_over)
+        print(text.game_over1)
     case "3":
         print("「え。。。」")
         input()
-        print(text.game_over)
+        print(text.game_over1)
+
+print(text.select2)
+select = input("数字を入力：")
+match select:
+    case "1":
+        print("「・・・・・ごめん、やっぱ無理。」")
+        input()
+        print(text.game_over2)
+    case "2":
+        print(text.scenario3)
+    case "3":
+        print("「・・・キモッ」")
+        input()
+        print(text.game_over2)
+        
+print(text.select3)
+select = input("数字を入力：")
+match select:
+    case "1":
+        print(text.scenario4)
+    case "2":
+        print("「早すぎ、信じらんない。」")
+        input()
+        print(text.game_over3)
+    case "3":
+        print("「ちょっと頭おかしいんじゃないの？」")
+        input()
+        print(text.game_over3)
+
+print(text.select4)
+select = input("数字を入力：")
+match select:
+    case "1":
+        print("「冗談じゃ。。。ないよ。。。」")
+        print("そういって彼女は教室を飛び出した。")
+        input()
+        print(text.game_over4)
+    case "2":
+        print("「そっか、、、」")
+        input()
+        print(text.game_over4)
+    case "3":
+        print(text.scenario5)
+
+print(text.select5)
+select = input("数字を入力：")
+match select:
+    case "1":
+        print("「あまいな、思えはもう死んでいる。」")
+        input()
+        print(text.game_over5)
+    case "2":
+        print("「雰囲気が台無しだよ。。。」")
+        input()
+        print(text.game_over5)
+    case "3":
+        print(text.scenario6)
